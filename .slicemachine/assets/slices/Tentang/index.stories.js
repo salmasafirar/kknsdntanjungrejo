a11e1ebb3e77dee486fe011fe188c02b6269d0b1
salmasafirar <salmasafira2111@gmail.com>
2023-01-21 13:41:33 +0700

    add tentang

--- .slicemachine/assets/slices/Tentang/index.stories.js
+++ .slicemachine/assets/slices/Tentang/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Eight","spans":[]}],"description":[{"type":"paragraph","text":"Nostrud ea consequat proident adipisicing amet qui sit non et ipsum ex amet.","spans":[]}]},"id":"_Default","slice_type":"tentang"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Eight","spans":[]}],"description":[{"type":"paragraph","text":"Nostrud ea consequat proident adipisicing amet qui sit non et ipsum ex amet.","spans":[]}],"image":{"dimensions":{"width":3810,"height":5715},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1587614295999-6c1c13675117"}},"id":"_Default","slice_type":"tentang"}} />
 _Default.storyName = ''
--- slices/Tentang/index.js
+++ slices/Tentang/index.js
@@ -1,36 +1,45 @@
-import React from 'react'
-import { PrismicRichText } from '@prismicio/react'
+import React from 'react';
+import { PrismicRichText } from '@prismicio/react';
+import { isFilled } from '@prismicio/helpers';
 
 /**
  * @typedef {import("@prismicio/client").Content.TentangSlice} TentangSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<TentangSlice>} TentangProps
  * @param { TentangProps }
  */
-const Tentang = ({ slice }) => (
-  <section>
-    <span className="title">
-      {
-        slice.primary.title ?
-        <PrismicRichText field={slice.primary.title}/>
-        : <h2>Template slice, update me!</h2>
-      }
-    </span>
-    {
-      slice.primary.description ?
-      <PrismicRichText field={slice.primary.description}/>
-      : <p>start by editing this slice from inside Slice Machine!</p>
-    }
-    <style jsx>{`
-        section {
-          max-width: 600px;
-          margin: 4em auto;
-          text-align: center;
-        }
-        .title {
-          color: #8592e0;
-        }
-    `}</style>
-  </section>
-)
+const Tentang = ({ slice }) => {
+	console.log(slice);
 
-export default Tentang
+	return (
+		<section>
+			<div>
+				<img className="" src={slice.primary.image.url} />
+			</div>
+			<span className="title">
+				{slice.primary.title ? (
+					<PrismicRichText field={slice.primary.title} />
+				) : (
+					<h2>Template slice, update me!</h2>
+				)}
+			</span>
+			{slice.primary.description ? (
+				<PrismicRichText field={slice.primary.description} />
+			) : (
+				<p>start by editing this slice from inside Slice Machine!</p>
+			)}
+			<style jsx>{`
+				section {
+					max-width: 600px;
+					margin: 4em auto;
+					text-align: center;
+				}
+				.title {
+					color: #8592e0;
+				}
+			`}</style>
+		</section>
+	);
+};
+
+export default Tentang;
+
